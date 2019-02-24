0a8d7b2ebc7d836bf110e75a116f23e76f518a5b
faruksyhn <farukseyhan08@hotmail.com>
2019-02-24 20:42:07 +0300

    Santranc
    Santranç tahtası üzerindeki atın hareketlerini kullanarak tüm kareleri gezmeye dayalı basit bir santranç oyunu

---
+++ Faruk Seyhan 152804008/Santranc/Santranc/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Santranc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.cmbBuyukluk = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(266, 12);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(75, 23);
+            this.btnOlustur.TabIndex = 1;
+            this.btnOlustur.Text = "Başla";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(25, 53);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // cmbBuyukluk
+            // 
+            this.cmbBuyukluk.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBuyukluk.FormattingEnabled = true;
+            this.cmbBuyukluk.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmbBuyukluk.Location = new System.Drawing.Point(88, 12);
+            this.cmbBuyukluk.Name = "cmbBuyukluk";
+            this.cmbBuyukluk.Size = new System.Drawing.Size(121, 21);
+            this.cmbBuyukluk.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Büyüklük";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 374);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBuyukluk);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnOlustur);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox cmbBuyukluk;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Faruk Seyhan 152804008/Santranc/Santranc/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Santranc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int ButonAdi = 0,sayac=0;
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            if (cmbBuyukluk.Text!="")
+            {
+                flowLayoutPanel1.Controls.Clear();
+                int kare = Convert.ToInt16(cmbBuyukluk.Text);
+                flowLayoutPanel1.Width = kare * 30;
+                flowLayoutPanel1.Height = kare * 30;
+                for (int i = 0; i < kare; i++)
+                {
+                    for (int j = 0; j < kare; j++)
+                    {
+                        ButonEkle(new Point(i, j));
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Büyüklük değerini giriniz.");
+            }
+        }
+        public void ButonEkle(Point loc)
+        {
+            //Dinamik buton oluştur.
+            sayac = 0;
+            Button btn = new Button();
+            btn.BackColor = Color.White;
+            btn.Size = new Size(30, 30);
+            flowLayoutPanel1.Controls.Add(btn);
+            btn.Margin = new Padding(0);
+            btn.Location = loc;
+            btn.Name = ButonAdi.ToString();
+            btn.Click += Btn_Click;
+            ButonAdi++;
+        }
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            sayac++;
+            int kare = Convert.ToInt16(cmbBuyukluk.Text);
+            int a = -1, b = -1, c = -1, d = -1;
+            Button btn = (sender as Button);
+            btn.Text = sayac.ToString();
+            for (int i = 0; i < kare*kare; i++)
+            {
+                if (flowLayoutPanel1.Controls[i].Name == btn.Name)
+                {
+                    if (i > (kare + kare - 1) && i % kare < kare - 1 && flowLayoutPanel1.Controls[i - (kare + kare - 1)].Text == "")//2 üst 1 sağ
+                    {
+                        flowLayoutPanel1.Controls[i - (kare + kare - 1)].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i - (kare + kare - 1)].Enabled = true;
+                    }
+                    if (i > (kare + kare - 1) && i % kare > 0 && flowLayoutPanel1.Controls[i - (kare + kare + 1)].Text == "")//2üst 1 sol
+                    {
+                        flowLayoutPanel1.Controls[i - (kare + kare + 1)].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i - (kare + kare + 1)].Enabled = true;
+                    }
+                    if (i > (kare - 1) && i % kare < kare - 2 && flowLayoutPanel1.Controls[i - (kare - 2)].Text == "")//1 üst 2 sağ
+                    {
+                        flowLayoutPanel1.Controls[i - (kare - 2)].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i - (kare - 2)].Enabled = true;
+                    }
+                    if (i > (kare - 1) && i % kare > 1 && flowLayoutPanel1.Controls[i - (kare + 2)].Text == "")//1 üst 2 sol
+                    {
+                        flowLayoutPanel1.Controls[i - (kare + 2)].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i - (kare + 2)].Enabled = true;
+                    }
+                    if (i < (kare*kare - (kare * 2)) && i % kare > 0 && flowLayoutPanel1.Controls[i + (kare + kare - 1)].Text == "")//2 alt 1 sol
+                    {
+                        flowLayoutPanel1.Controls[i + kare + kare - 1].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i + kare + kare - 1].Enabled = true;
+                        a = i + kare + kare - 1;
+                    }
+                    if (i < (kare * kare - (kare * 2)) && i % kare < kare-1 && flowLayoutPanel1.Controls[i + (kare + kare + 1)].Text == "")//2 alt 1 sağ
+                    {
+                        flowLayoutPanel1.Controls[i + kare + kare + 1].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i + kare + kare + 1].Enabled = true;
+                        b = i + kare + kare + 1;
+                    }
+                    if (i < (kare * kare - kare) && i % kare > 1 && flowLayoutPanel1.Controls[i + (kare - 2)].Text == "")//1 alt 2 sol
+                    {
+                        flowLayoutPanel1.Controls[i + kare - 2].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i + kare - 2].Enabled = true;
+                        c = i + kare - 2;
+                    }
+                    if (i < (kare * kare - kare) && i % kare < kare-2 && flowLayoutPanel1.Controls[i + kare + 2].Text == "")//1 alt 2 sağ
+                    {
+                        flowLayoutPanel1.Controls[i + kare + 2].BackColor = Color.Green;
+                        flowLayoutPanel1.Controls[i + kare + 2].Enabled = true;
+                        d = i + kare + 2;
+                    }
+                    if (i==kare*kare)
+                    {
+                        MessageBox.Show("Oyun Bitti!");
+                    }
+                }
+                if (i != a && i != b && i != c && i != d)
+                {
+                    flowLayoutPanel1.Controls[i].Enabled = false;
+                    flowLayoutPanel1.Controls[i].BackColor = Color.White;
+                }
+            }
+            int s = 0, kazandin = 0; ;
+            for (int i = 0; i < kare * kare; i++)
+            {
+                if (flowLayoutPanel1.Controls[i].BackColor != Color.Green)
+                {
+                    s++;
+                }
+                if (s==kare*kare)
+                {
+                    MessageBox.Show("Kaybettin!");
+                }
+                if (flowLayoutPanel1.Controls[i].Text != "")
+                {
+                    kazandin++;
+                }
+                if (kazandin==kare*kare)
+                {
+                    MessageBox.Show("Tebrikler Kazandınız!");
+                }
+            }
+            for (int i = 0; i < kare*kare; i++)
+            {
+                if (flowLayoutPanel1.Controls[i].Text != "")
+                {
+                    flowLayoutPanel1.Controls[i].BackColor = Color.Orange;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
